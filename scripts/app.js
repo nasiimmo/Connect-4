7a7bfe422c9ce832a95b1ed9933d3e95138a3263
Nasiim Nuur <nasiimmo.691@gmail.com>
2023-11-06 15:58:06 +0000

    added divs to grid and simplified pseudocode

--- scripts/app.js
+++ scripts/app.js
@@ -1,99 +1,99 @@
+
 // ! Elements 
-//Player1 = A variable to store the player1
+const player1Score = document.querySelector('.current-score1')
+const player2Score = document.querySelector('.current-score2')
 
-//Player2 = A variable to store the player2
-
-//current player = A variable to store the current player
-
-//set game status to false
-//--is a variable to indicate whether the game is on going (true) or over(false)
-
-//Start Button which starts the whole game
-
-//Play again button 
-
-//When game decides its winner or draw end the game 
-
-// ! Variables
-//current player is a variable to store a current player ('r' or 'y')
-const player1 = 'red'
-const player2 = 'yellow'
-const currentPlayer = player1
-const gameOver = false
-//const board = createEmptyBoard(rows, columns)
+// Variables
+const player1 = 'R'
+const player2 = 'Y'
+let currentPlayer = player1
+let gameOver = false
+let board = [] // You need to initialize the board as an empty array
 const rows = 6
 const columns = 7
 const startBtn = document.querySelector('#start-btn')
-const playAgainBtn = document.querySelector('#retart-Btn')
+const playAgainBtn = document.querySelector('#restart-btn') // Correct the typo
 
-// ! Executions
-//if start button is clicked start the game
-startBtn.addEventLister('click', startGame)
+const grid = document.querySelector('#board')
+const cells = []
 
+let currentPos = 0
 
-//This is where the game starts after start button is pressed
-function startGame() {
+// Grid
+const width = 7
+const height = 6
+const cellCount = width * height
 
+// Function to create the grid
+function createGrid() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.innerText = i
+    cell.id = i
+    cell.style.width = `${90 / width}%`
+    grid.append(cell)
+    cells.push(cell)
+    if (i < 7) {
+      cell.addEventListener('click', currentCell)
+    } 
+    const columnNumber = i % width
+    cell.classList.add(`col-${columnNumber}`)
+  }
 }
 
-//function addcolor(){}
-//display player 1 color red
-//display color 2 yellow
+function currentCell(){
+  console.log('hi')
+}
 
-// * grid
-//Use a (for) loop to create a row and column with empty string
-//create a for loop for row and column this will append them to the existing grid
-//row.push() this is to a empty circle that pushes to the end of my grid
+//if top row is clicked add class to next available space from down up
+//does it have color class already if it doesnt add class if it doesnt check next array
 
-//document.createElement('div') - this creates a new div into my html
-//how will i know what what column i licked on
-//if id is clicked then check the row for any 
-//if index[30] have a class of idex 0 if it does then check next until it is empty
-//cell index that is empty add the class red
-//repeat for all column that has been clicked
-//what are the indexes make up the column i clicked on
-//use the push function to push the cells onto the board
+//Add event listeners to the top row cells in the columns
 
-//onlick update depending on row that has been clicked
-//make a const row that will find the available space on the board within the row, when a column is clicked
-//make a function to find an available space in that row
-//add an event listener for the column clicked
-//when any top column is selected the player can iterate through the rows
-//in that column to find the first available empty cell from the bottom up and add current players turn color,
-//update whats displayed when the available space is found and color is added
-//check for win condition
-//if no one has won or drew, toggle to next player for their turn
+//how will i know what column i clicked on
 
-
-
-//Make each starting point whatever row player wants within the grid
-//Make sure when div is clicked,
-//color is dropped the next avaialable space in the row, repeat for player 1 and 2 until board is full or winner is declared
-//if 
-//make sure game works as you go through
-
-//when player 1 go is finished, start player 2 and repeat 
-//till winner is declared or draw is declared
-
-
-// ! Win condtion 
-//function checkWinCondtion
-//need to know when a player has 4 in a row, horizonatally, vertically, or diagonally.
-
-//Check diagnal for win both ways
-//Check row and column for a win 
-//if checkWin(board, currentPlayer)
-//print "Player" + currentPlayer + ' IS THE WINNER'
-
+// Listen for clicks on the grid cells.
+// When a cell is clicked, find an available space in that column
+// Add the current player's color to the first available empty cell in the column.
+// Check for a win condition.
+// make a function to check for a win - diagnally aswell
+// - If no win, draw, or game-over, toggle to the next player for their turn.
 //if the board is full
 //print "Its a draw"
 
+
+// How will the board know where to find an available space in a column
+
+// To find an available space in a column:
+// - Start from the bottom row.
+// - Iterate upwards until an empty cell is found.
+// - Mark that cell with the current player's color.
+// create a function to find the empty row index for a given column
+
+
+// Switch to the other player
+// Add event listener to handle player choices
+
+// make red and yellow color appear based on whos turn it is
+
+
+
+
+// Start button click event listener
+startBtn.addEventListener('click', startGame)
+
+function startGame() {
+// Initialize the game board
+}
+
+
 // *  bonus
 //when game ends add points to winner of previous game.
-
+//how do i get a modal to pop up after gameis finished
+//add a gif as background
 //Play again button which clears the board and restarts.
 
-// ! Events
-//PlayAgain.addEventLister('click', resetGame)
-
+// Clear the grid
 // ! page Load
+//make createGrid to create the initial game grid
+createGrid()
