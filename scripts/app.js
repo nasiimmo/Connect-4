a3c4b271db62eab0d6a2884562f8e168c8d64d3e
Nasiim Nuur <nasiimmo.691@gmail.com>
2023-11-06 16:57:03 +0000

    selected the columns in my grid based on the column index and get their ID

--- scripts/app.js
+++ scripts/app.js
@@ -23,28 +23,78 @@ let currentPos = 0
 const width = 7
 const height = 6
 const cellCount = width * height
-
+createGrid()
 // Function to create the grid
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement('div')
+    if (i < 7) {
+      cell.addEventListener('click', currentCell)
+    }
+    const columnNumber = i % width
+    cell.classList.add(`col-${columnNumber}`)
     cell.innerText = i
     cell.id = i
     cell.style.width = `${90 / width}%`
     grid.append(cell)
     cells.push(cell)
-    if (i < 7) {
-      cell.addEventListener('click', currentCell)
-    } 
-    const columnNumber = i % width
-    cell.classList.add(`col-${columnNumber}`)
   }
 }
-
-function currentCell(){
+function currentCell(index) {
   console.log('hi')
 }
+//get all 7 columns, based on what index i pressed i need to know what column i selected and how do i get the id based on what imdex i clicked on top row.
+const colZero = document.querySelectorAll('.col-0')
+colZero.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 0,ID: ${columnIndex}`)
+  })
+})
+const colOne = document.querySelectorAll('.col-1')
+colOne.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 1,ID: ${columnIndex}`)
+  })
+})
+const colTwo = document.querySelectorAll('.col-2')
+colTwo.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 2,ID: ${columnIndex}`)
+  })
+})
+const colThree = document.querySelectorAll('.col-3')
+colThree.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 3,ID: ${columnIndex}`)
+  })
+})
+const colFour = document.querySelectorAll('.col-4')
+colFour.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 4,ID: ${columnIndex}`)
+  })
+})
+const colFive = document.querySelectorAll('.col-5')
+colFive.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 5,ID: ${columnIndex}`)
+  })
+})
+const colSix = document.querySelectorAll('.col-6')
+colSix.forEach((element) => {
+  element.addEventListener('click', () => {
+    const columnIndex = parseInt(element.innerText)
+    console.log(`clicked on column 6,ID: ${columnIndex}`)
+  })
+})
 
+//review lessons about: for loops 
 //if top row is clicked add class to next available space from down up
 //does it have color class already if it doesnt add class if it doesnt check next array
 
@@ -83,7 +133,7 @@ function currentCell(){
 startBtn.addEventListener('click', startGame)
 
 function startGame() {
-// Initialize the game board
+  // Initialize the game board
 }
 
 
@@ -96,4 +146,4 @@ function startGame() {
 // Clear the grid
 // ! page Load
 //make createGrid to create the initial game grid
-createGrid()
+
