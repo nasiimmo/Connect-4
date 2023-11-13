c4f26888f70804c7187a74c3440c0fddc079ec6b
Nasiim Nuur <nasiimmo.691@gmail.com>
2023-11-13 11:02:02 +0000

    attempt making win condtion

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@
 const player1 = 'R'
 const player2 = 'Y'
 let currentPlayer = player1
-let gameOver = false
+
 // You need to initialize the board as an empty array
 const rows = 6
 const columns = 7
@@ -36,7 +36,7 @@ function createGrid() {
     if (i < 7) {
       cell.addEventListener('click', currentCell)
     }
-    const rowNumber = Math.floor( i / width)
+    const rowNumber = Math.floor(i / width)
     cell.dataset.row = rowNumber
     const columnNumber = i % width
     cell.dataset.col = columnNumber
@@ -51,54 +51,12 @@ function createGrid() {
 }
 
 function currentCell() {
-  console.log('hi')
+  console.log('hello')
 }
 
 function endGame(message) {
   console.log(message)
 }
-
-//Get all 7 columns, based on what index i pressed i need to know what column i selected and how do i get the id based on what imdex i clicked on top row. whatever i do for column 0 i will repeat for all 7 columns
-
-// const colZero = document.querySelectorAll('.col-0')
-// //This block of code sets up an event listener for each cell in the first column ('col-0'). It listens for a click on any of these cells.
-// colZero.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const columnIndex = parseInt(element.innerText)
-//     console.log(`clicked on column 0,ID: ${columnIndex}`)
-//     if (columnIndex === 0) {
-//       const cellsInColumn = document.querySelectorAll('.col-0')
-//       let targetCell = null
-//       // Iterate from the bottom to the top in 'column 0'
-//       //I set up a loop to iterate over the cells in 'col-0' from the bottom (last cell) to the top (first cell).
-//       for (let i = cellsInColumn.length - 1; i >= 0; i--) {
-//         if (!cellsInColumn[i].classList.contains('occupied')) {
-//           //Within the loop, i checked if the current cell is not already marked as 'occupied.' If it's not taken, it's a valid space to place the player's piece.
-//           targetCell = cellsInColumn[i]
-//           targetCell.classList.add('occupied')
-//           //This code shows that the space is now taken^
-//           console.log(`Added class to cell ID: ${targetCell.id}`)
-//           //This intitiates players turn and color
-//           if (currentPlayer === 1) {
-//             targetCell.style.backgroundColor = 'yellow'
-//           } else {
-//             targetCell.style.backgroundColor = 'red'
-//           }
-//           // - If no win, draw, or game-over, toggle to the next player for their turn.
-//           // * if (draw !== true && no one has won) return next below as said below
-//           // Toggle the current player for the next move
-//           //After each move, we toggle the currentPlayer between 1 and 2, ensuring that it switches to the other player's turn for the next move.
-//           currentPlayer = currentPlayer === 1 ? 2 : 1
-//           // This switches the value of 'currentPlayer' between 1 and 2. If it's currently 1, it becomes 2, and if it's currently 2 or any other value, it becomes 1. 
-//           if (isDraw()){
-//             endGame('draw')
-//           }
-//           break // This exits the loop after adding the class to the first available space, this prevents placing multiple cells in a single move.
-//         }
-//       }
-//     }
-//   })
-// })
 
 function isDraw() {
 
@@ -111,193 +69,73 @@ function isDraw() {
 }
 
 
-//function checkForWin() {
-//console.log('hi')
-//check every column, row and diagnoly for a win by using for loop and if statement
-//}
-//checkForWin()
+function isWin(colCells) {
+  console.log('hi')
+  //check every column, row and diagnally, other-diagnal for a win by using for loop and if statement
+  console.log(colCells)
+  //check column cells for 4 in a row
+  //check each cell for class of occupied
+  for (let i = 0; i < colCells.length; i++) {
+    if (colCells[i].classList.contains('occupied')) {
+      console.log('contains counter')
+    } else {
+      console.log('no counter')
+    }
+    // if class has occupied add 1 to the cell and if its 4 right next to each other in a row currrent player has won
+  }
+  
+  for (let i = 0; i <= colCells.length - 4; i++) {
+    const currentCell = colCells[i]
+    if (
+      currentCell.classList.contains('occupied') &&
+      currentCell.dataset.player === colCells[i + 1].dataset.player &&
+      //Checks if the player in the current cell is the same as the player in the next cell and so on.
+      currentCell.dataset.player === colCells[i + 2].dataset.player &&
+      currentCell.dataset.player === colCells[i + 3].dataset.player
+    ) {
+      console.log(`Player ${currentCell.dataset.player} wins!`)
+      return true
+    }
+  }
+  console.log('No player has won in this column.')
+  return false
+}
 
-//repeated for all 7 columns
 
 
 
-// const colOne = document.querySelectorAll('.col-1')
-// colOne.forEach((element) => {
-//   element.addEventListener('click', takeTurn)
-// })
+
 
 function takeTurn(event) {
   const element = event.target
   const columnIndex = parseInt(element.dataset.col)
-  console.log(columnIndex)
+  //console.log(columnIndex)
   const cellsInColumn = document.querySelectorAll(`.col-${columnIndex}`)
-  console.log(cellsInColumn)
+  const cellsInRow = document.querySelectorAll(`.col-${columnIndex}`)
+  const cellsDiagonally1 = document.querySelectorAll(`.col-${columnIndex}`)
+  const cellsDiagonally2 = document.querySelectorAll(`.col-${columnIndex}`)
+  //console.log(cellsInColumn)
   let targetCell = null
   for (let i = cellsInColumn.length - 1; i >= 0; i--) {
     if (!cellsInColumn[i].classList.contains('occupied')) {
       targetCell = cellsInColumn[i]
       targetCell.classList.add('occupied')
-      if (currentPlayer === 1) {
-        targetCell.style.backgroundColor = 'yellow'
-      } else {
+
+      if (currentPlayer === 'R') {
         targetCell.style.backgroundColor = 'red'
+      } else {
+        targetCell.style.backgroundColor = 'yellow'
       }
-      currentPlayer = currentPlayer === 1 ? 2 : 1
-      if (isDraw()){
+      if (isWin(cellsInColumn, cellsInRow, cellsDiagonally1, cellsDiagonally2)) {
+        endGame(`Player ${currentPlayer} is the winner !`)
+      } else if (isDraw()) {
         endGame('draw')
       }
+      currentPlayer = currentPlayer === 'R' ? 'Y' : 'R'
       break
     }
   }
 }
-
-// const colTwo = document.querySelectorAll('.col-2')
-// colTwo.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const columnIndex = parseInt(element.innerText)
-//     if (columnIndex === 2) {
-//       const cellsInColumn = document.querySelectorAll('.col-2')
-//       let targetCell = null
-//       for (let i = cellsInColumn.length - 1; i >= 0; i--) {
-//         if (!cellsInColumn[i].classList.contains('occupied')) {
-//           targetCell = cellsInColumn[i]
-//           targetCell.classList.add('occupied')
-//           if (currentPlayer === 1) {
-//             targetCell.style.backgroundColor = 'yellow'
-//           } else {
-//             targetCell.style.backgroundColor = 'red'
-//           }
-//           currentPlayer = currentPlayer === 1 ? 2 : 1
-//           if (isDraw()){
-//             endGame('draw')
-//           }
-//           break
-//         }
-//       }
-//     }
-//   })
-// })
-
-
-
-// const colThree = document.querySelectorAll('.col-3')
-// colThree.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const columnIndex = parseInt(element.innerText)
-//     if (columnIndex === 3) {
-//       const cellsInColumn = document.querySelectorAll('.col-3')
-//       let targetCell = null
-//       for (let i = cellsInColumn.length - 1; i >= 0; i--) {
-//         if (!cellsInColumn[i].classList.contains('occupied')) {
-//           targetCell = cellsInColumn[i]
-//           targetCell.classList.add('occupied')
-//           if (currentPlayer === 1) {
-//             targetCell.style.backgroundColor = 'yellow'
-//           } else {
-//             targetCell.style.backgroundColor = 'red'
-//           }
-//           currentPlayer = currentPlayer === 1 ? 2 : 1
-//           if (isDraw()){
-//             endGame('draw')
-//           }
-//           break
-//         }
-//       }
-//     }
-//   })
-// })
-
-
-
-
-// const colFour = document.querySelectorAll('.col-4')
-// colFour.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const columnIndex = parseInt(element.innerText)
-//     if (columnIndex === 4) {
-//       const cellsInColumn = document.querySelectorAll('.col-4')
-//       let targetCell = null
-//       for (let i = cellsInColumn.length - 1; i >= 0; i--) {
-//         if (!cellsInColumn[i].classList.contains('occupied')) {
-//           targetCell = cellsInColumn[i]
-//           targetCell.classList.add('occupied')
-//           if (currentPlayer === 1) {
-//             targetCell.style.backgroundColor = 'yellow'
-//           } else {
-//             targetCell.style.backgroundColor = 'red'
-//           }
-//           currentPlayer = currentPlayer === 1 ? 2 : 1
-//           if (isDraw()){
-//             endGame('draw')
-//           }
-//           break
-//         }
-//       }
-//     }
-//   })
-// })
-
-
-
-// const colFive = document.querySelectorAll('.col-5')
-// colFive.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const columnIndex = parseInt(element.innerText)
-//     if (columnIndex === 5) {
-//       const cellsInColumn = document.querySelectorAll('.col-5')
-//       let targetCell = null
-//       for (let i = cellsInColumn.length - 1; i >= 0; i--) {
-//         if (!cellsInColumn[i].classList.contains('occupied')) {
-//           targetCell = cellsInColumn[i]
-//           targetCell.classList.add('occupied')
-//           if (currentPlayer === 1) {
-//             targetCell.style.backgroundColor = 'yellow'
-//           } else {
-//             targetCell.style.backgroundColor = 'red'
-//           }
-//           currentPlayer = currentPlayer === 1 ? 2 : 1
-//           if (isDraw()){
-//             endGame('draw')
-//           }
-//           break
-//         }
-//       }
-//     }
-//   })
-// })
-
-
-// const colSix = document.querySelectorAll('.col-6')
-// colSix.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     const columnIndex = parseInt(element.innerText)
-//     if (columnIndex === 6) {
-//       const cellsInColumn = document.querySelectorAll('.col-6')
-//       let targetCell = null
-//       for (let i = cellsInColumn.length - 1; i >= 0; i--) {
-//         if (!cellsInColumn[i].classList.contains('occupied')) {
-//           targetCell = cellsInColumn[i]
-//           targetCell.classList.add('occupied')
-//           if (currentPlayer === 1) {
-//             targetCell.style.backgroundColor = 'yellow'
-//           } else {
-//             targetCell.style.backgroundColor = 'red'
-//           }
-//           currentPlayer = currentPlayer === 1 ? 2 : 1
-//           if (isDraw()){
-//             endGame('draw')
-//           }
-//           break
-//         }
-//       }
-//     }
-//   })
-// })
-
-
-//points counter
-//pointsCounter.innerText = Number(pointsCounter.innerText) + 100
-//Review lessons about: for loops 
 
 
 // Listen for clicks on the grid cells.
